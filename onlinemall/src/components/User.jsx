2e7bad5258b457e19e29c1a6f0f8973a9c38d9d2
YUHASUNG <yuha822@naver.com>
2023-01-19 21:04:33 +0900

    dosc: 사용자 아바타 설정

--- onlinemall/src/components/User.jsx
+++ onlinemall/src/components/User.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 // {user} 안에 유저 정보 데이터 받아옴
 export default function User({user: {photoURL, displayName}}) {
-    return ( 
+    return (
         <div className='flex items-center'>
             <img 
                 className='w-10 h-10 rounded-full mr-2' 
                 src={photoURL} 
                 alt={displayName} 
             />
+            {/* 반응형 / 사이즈가 미디엄이 되면 displayName이 숨어있지 말고 나타남 */}
             <span className='hidden md:block'>{displayName}</span>
         </div> 
     );
