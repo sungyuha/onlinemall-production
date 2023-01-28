02a3f95f1736ebea76d4ebf6701b8af3c6f1aa5a
YUHASUNG <yuha822@naver.com>
2023-01-28 23:15:02 +0900

    dosc: firebase 재셋업

--- onlinemall/src/api/firebase.js
+++ onlinemall/src/api/firebase.js
@@ -9,10 +9,14 @@ import {
 import { getDatabase, ref, child, get } from "firebase/database";
 
 const firebaseConfig = {
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // databaseURL: process.env.REACT_APP_FIREBASE_FIREBASEURL,
+    // projectId: process.env.REACT_APP_FIREBASE_FIREBASE_PROJECT_ID,
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_FIREBASEURL,
-    projectId: process.env.REACT_APP_FIREBASE_FIREBASE_PROJECT_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 // Initialize Firebase
