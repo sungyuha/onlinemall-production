d90c5b55c98797dcc80b65af148fb2444c8eba83
YUHASUNG <yuha822@naver.com>
2023-02-13 22:08:33 +0900

    dosc: admin 사용자 경로 보호

--- onlinemall/src/pages/ProtectedRoute.jsx
+++ onlinemall/src/pages/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ProtectedRoute(chilren, requireAdmin) {
   // 로그인 한 사용자가 있는지 확인
-  
+  // const { user } = useAuthContext();
   // 그 사용자가 어드민 권한이 있는지 확인
 
   // requireAdmin 이 true 인 경우에는 로그인도 되어 있어야 하고, 어드민 권한도 가지고 있음
