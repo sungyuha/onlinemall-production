7bbfb2ce20e52e2bbf2a5f370dfd1b526b390fb1
YUHASUNG <yuha822@naver.com>
2023-02-19 21:51:33 +0900

    dosc: 새로운 재품 등록 입력폼

--- onlinemall/src/pages/NewProduct.jsx
+++ onlinemall/src/pages/NewProduct.jsx
@@ -4,7 +4,10 @@ export default function NewProduct() {
 
   const [product, setProjduct] = useSteate({});
   const handleSubmit = (e) => {
-
+    // 
+    const {name, value, files} = e.target;
+    // 이전의 product 값을 받아와서 
+    setProjduct((product) => ({...product, [name]:value})); // 새로운 오브젝트인 product와 입력 폼에서 변경이 발생한 name의 key에 value를 할당해줌
   };
   const handleChange = (e) => {
     
