87d3e27415b232cf1d2c048956988511bfd08829
YUHASUNG <yuha822@naver.com>
2023-02-23 21:57:27 +0900

    dosc: 이미지 업로드

--- onlinemall/src/api/uploader.js
+++ onlinemall/src/api/uploader.js
@@ -5,4 +5,11 @@ export async function uploadImage(file) {
     data.append('file', file);
     // 업로드
     data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
+    // URL
+    fetch(process.env.REACT_APP_CLOUDINARY_URL, {
+        // 데이터 전달
+        method: 'post',
+        // NewProduct 폼 데이터
+        body: data
+    })
 }
