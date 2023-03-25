627d9584dc9fdeb160a8d15aa62423639ff945d9
YUHASUNG <yuha822@naver.com>
2023-03-25 12:28:57 +0900

    dosc: 상품 조회회

--- onlinemall/src/pages/AllProducts.jsx
+++ onlinemall/src/pages/AllProducts.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import Products from '../components/Products';
 
 export default function AllProducts() {
+  // 상품 조회 화면 - 현재는 아무런 데이터가 없어서 Not Found 출력 됨
   return <Products />;
 }
