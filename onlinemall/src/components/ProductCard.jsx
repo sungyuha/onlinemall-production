55a06f46623626687148783c31b403e9d2646b2e
YUHASUNG <yuha822@naver.com>
2023-03-31 13:18:55 +0900

    dosc: 상품 조회 UI

--- onlinemall/src/components/ProductCard.jsx
+++ onlinemall/src/components/ProductCard.jsx
@@ -5,15 +5,15 @@ export default function ProductCard({
     product: {id, image, title, category, price}, // product을 낱개로 풀어서 가져올 수 있음
 }) {
     return (
-        <li className='rounded-lg shadow-md overflow-hidden cursor-porint'>
+        <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
             {/* 이미지 */}
             <img className='w-full' src={image} alt={title} />
             {/* 메타 데이터 정보 */}
-            <div>
+            <div className='mt-2 px-2 text-lg s flex justify-between items-content'>
                 {/* 제목 */}
-                <h3>{title}</h3>
+                <h3 className='truncate'>{title}</h3>
                 {/* 한국 원화 표시가 된 가격 */}
-                <p>{`₩price`}</p> 
+                <p className='mb-2 px-2 text-gray-600'>{`₩price`}</p> 
             </div>
         </li>
     )
