31c7d493f526b51f42d6011a7e7438aca6d83c60
YUHASUNG <yuha822@naver.com>
2023-04-02 18:56:30 +0900

    dosc: 상품 조회 UI

--- onlinemall/src/pages/Home.jsx
+++ onlinemall/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+// import Products from '../components/Products';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      Home
+      {/* <Products /> */}
+    </>
+  )
 }
