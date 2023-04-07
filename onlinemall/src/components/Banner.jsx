46478613d97281c63826578915c98e0b24e01d5d
YUHASUNG <yuha822@naver.com>
2023-04-07 14:28:57 +0900

    dosc: 상품 조회 UI

--- onlinemall/src/components/Banner.jsx
+++ onlinemall/src/components/Banner.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function Banner() {
-    return <section>
+    return <section className='h-96 bg-yellow-900 relative'>
         {/* <div> 상품 설명 </div> */}
-        <div>
+        <div className='absolute w-full top-32 text-center text-gray-100'>
             <h2>shoppy 상품</h2>
             <p>베스트 상품</p>
             
