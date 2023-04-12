d18d990b4698af713ec9b85eea88b6d45b953e9a
YUHASUNG <yuha822@naver.com>
2023-04-12 15:34:59 +0900

    dosc: 상품 조회 UI

--- onlinemall/src/components/Banner.jsx
+++ onlinemall/src/components/Banner.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function Banner() {
     return <section className='h-96 bg-yellow-900 relative'>
-        <div className='w-full h-full bg-cover bg-hanner opacity-75' />
+        {/* width와 height full 사이즈, background-cover */}
+        <div className='w-full h-full bg-cover bg-hanner opacity-85' />
         <div className='absolute w-full top-32 text-center text-gray-100'>
             <h2 className='text-6xl'>shoppy 상품</h2>
             <p className='text-2xl'>베스트 상품</p>
