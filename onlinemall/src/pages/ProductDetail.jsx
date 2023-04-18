4439ff4520d23e9412e85f3f64d0832341afbfae
YUHASUNG <yuha822@naver.com>
2023-04-18 13:02:47 +0900

    dosc: 제품 상세 조회 UI

--- onlinemall/src/pages/ProductDetail.jsx
+++ onlinemall/src/pages/ProductDetail.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function ProductDetail() {
+  const {
+    // 전달 받은 state를 react-router-dom에서 product으로 낱개로 풀어서 가져옴
+    state: {
+      product: {id, image, title, description, categroy, price, options}
+    }
+  } = useLocation(); // useLocation를 통해서 받아올 수 있음
   return (
     <div>Product Details</div>
   )
