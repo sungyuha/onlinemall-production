5f8166e6e0d72775dee705ae4e6b5bf64f3ffa64
YUHASUNG <yuha822@naver.com>
2023-05-15 12:54:15 +0900

    dosc: 장바구니 기능 추가

--- onlinemall/src/components/CartStatus.jsx
+++ onlinemall/src/components/CartStatus.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { AiOutlineShoppingCart} from 'react-icons/ai'
+import { AiOutlineShoppingCart} from 'react-icons/ai';
+import {getCart} from '../api/firebase';
+import {useQuery} from '@tanstack/react-query';
 
 export default function CartStatus() {
+    // useQuery 의 키는 carts, 함수는 getCart
+    const {data: produtes} = useQuery(['carts'], getCart);
     return (
         <div>
             {/* 카트 아이콘 */}
