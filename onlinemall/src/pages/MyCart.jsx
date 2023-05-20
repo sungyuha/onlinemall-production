a774bef2e1daaef07abd514ecd87106421f1567c
YUHASUNG <yuha822@naver.com>
2023-05-20 18:59:56 +0900

    dosc: 장바구니

--- onlinemall/src/pages/MyCart.jsx
+++ onlinemall/src/pages/MyCart.jsx
@@ -4,6 +4,17 @@ import {useQuery} from '@tanstack/react-query';
 import {useAuthContext} from '../context/AuthContext';
 
 export default function MyCart() {
+  // 사용자안에 있는 uid
+  const {uid} = useAuthContext();
+
+  // useQuery 의 키는 carts, 함수는 getCart에 uid 전달
+  const {isLoading, data: produtes} = useQuery(['carts'], () => getCart(uid));
+
+  // 만약에 isLoading이라면 바로 rturn 
+  if(isLoading)return <p>Loading</p>;
+
+  // 로딩이 아닌 경우
+
   return (
     // 로그인 후 장바구니 버튼 누르면
     <div>My Cart</div>
