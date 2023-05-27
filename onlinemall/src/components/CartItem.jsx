137180b80f3a97eb5f46b0fb547d749a1541f7a1
YUHASUNG <yuha822@naver.com>
2023-05-27 19:47:21 +0900

    dosc: 장바구니

---
+++ onlinemall/src/components/CartItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CartItem({product}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- onlinemall/src/pages/MyCart.jsx
+++ onlinemall/src/pages/MyCart.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {getCart} from '../api/firebase';
 import {useQuery} from '@tanstack/react-query';
 import {useAuthContext} from '../context/AuthContext';
+import CartItem from '../components/CartItem';
 
 export default function MyCart() {
   // 사용자안에 있는 uid
@@ -27,13 +28,17 @@ export default function MyCart() {
       {/* 장바구니 안에 아무것도 담겨 있지 않다면 */}
       {!hasProducts && <p>장바구니에 상품이 없습니다.</p>}
       {/* 장바구니 안에 상품이 있다면 */}
-      {hasProducts && <>
-        <ul>
-          {/* 모든 제품의 정보를 보여줌 */}
-          {/* products이 있다면 products을 map할건데 각 각의 제품을 CartItem 으로 만들어서 보여줌 */}
-          {/* {products && products.map(product) => <CartItem key={product.id} } /> */}
-        </ul>
-      </>}
+      {hasProducts && ( 
+        <>
+          <ul>
+            {/* 모든 제품의 정보를 보여줌 */}
+            {/* products이 있다면 products을 map할건데 각 각의 제품을 CartItem 으로 만들어서 보여줌 */}
+            {products && products.map((product) => (
+              <CartItem key={product.id} product={product} />
+            ))}
+          </ul>
+        </>
+      )}  
     </section>
   )
 }
