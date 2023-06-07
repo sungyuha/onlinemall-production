fe42021c8ede3388c386a129856aa709cdd6f37c
YUHASUNG <yuha822@naver.com>
2023-06-07 20:16:21 +0900

    dosc: 나의 장바구니

--- onlinemall/src/components/CartItem.jsx
+++ onlinemall/src/components/CartItem.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-export default function CartItem({product}) {
+export default function CartItem({product, product: {id, image, title, option, quantity, price}}) {
     return (
         <li>
             {/* 처음에는 상품 이미지 */}
-            
+            <img src={image} alt={title} />
+            {/* 상품 정보 */}
+            <div>
+                <p>{title}</p>
+                <p>{option}</p>
+            </div>
         </li>
     )
 }
