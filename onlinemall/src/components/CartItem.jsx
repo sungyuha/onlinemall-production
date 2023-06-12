648ecfc76c87cb5dab3648a3c0d8632d82fcdaa1
YUHASUNG <yuha822@naver.com>
2023-06-12 19:50:48 +0900

    dosc: 나의 장바구니

--- onlinemall/src/components/CartItem.jsx
+++ onlinemall/src/components/CartItem.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import {AiOutlinePlusSquare, AiOutlineMinusSquare} from 'react-icons/ai';
+import {RiDeleteBin5Fill} from 'react-icons/ri';
+import { addOrUpdateToCart } from "../api/firebase";
 
 export default function CartItem({product, product: {id, image, title, option, quantity, price}}) {
+    
+    // 함수가 호출이 되면 
+    const handleMinus = () => {
+        // 1개만 있을 때 
+        if(quantity <2) return;
+        // 1개 이상인 경우
+        addOrUpdateToCart()
+    }
+    const handlePlus = () => {
+
+    }
+    const handleDelte = () => {
+
+    }
+
     return (
         <li>
             {/* 처음에는 상품 이미지 */}
@@ -9,6 +27,13 @@ export default function CartItem({product, product: {id, image, title, option, q
             <div>
                 <p>{title}</p>
                 <p>{option}</p>
+                <div>
+                    {/* 수량 아이콘 */}
+                    <AiOutlineMinusSquare onClick={handleMinus} /> {/* 마이너스 */}
+                    <span>{quantity}</span>
+                    <AiOutlinePlusSquare onClick={handlePlus} /> {/* 플러스 */}
+                    <RiDeleteBin5Fill onClick={handleDelte} /> {/* 삭제 */}
+                </div>
             </div>
         </li>
     )
