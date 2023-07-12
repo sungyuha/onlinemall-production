7b345638ee75c7fe7642eb61d76398215b1c7e27
YUHASUNG <yuha822@naver.com>
2023-07-12 20:20:04 +0900

    dosc: firebase update 및 UI 수정

--- onlinemall/src/pages/Home.jsx
+++ onlinemall/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Products from '../components/Products';
+import Products from '../components/Products';
 
 export default function Home() {
   return (
     <>
       Home
       {/* 상품 조회 화면 */}
-      {/* <Products /> */}
+      <Products />
     </>
   );
 }
