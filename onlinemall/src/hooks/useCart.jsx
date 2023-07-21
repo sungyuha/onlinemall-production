cdc0748463703885b73a47f7306a579b5ca9a3f9
YUHASUNG <yuha822@naver.com>
2023-07-22 00:03:21 +0900

    dosc: 장바구니

--- onlinemall/src/hooks/useCart.jsx
+++ onlinemall/src/hooks/useCart.jsx
@@ -1,14 +1,24 @@
-import { useQueryClient } from "@tanstack/react-query";
-import {useAuthContext} from "../context/AuthContext";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getCart } from '../api/firebase';
+import { useAuthContext } from "../context/AuthContext";
 
 export default function useCart() {
     // useAuthContext로 현재 사용자의 uid를 받아옴
     const {uid} = useAuthContext();
     // queryClient 정의
     const queryClient = useQueryClient();
+    /* 
+    1) Cart를 읽어오는건 caryQuery로 useQuery 사용
+    2) carts 인데 사용자별로 캐싱이 이루어지도록 uid 설정
+    3) 그런데 사용자가 로그인을 하지 않으면 쿼리가 실행되지 않도록 enabled 옵션 사용
+    */
+    const caryQuery = useQuery(['carts', uid || ''], () => getCart(uid), {
+        enabled: !!uid,
+    });
+
     return (
         <div>
-            
+
         </div>
     );
 };
