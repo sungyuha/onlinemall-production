b9e5b5519cef54dca1c4f6512bb5f36304f0dbd5
YUHASUNG <yuha822@naver.com>
2023-07-30 18:36:14 +0900

    dosc: 업데이트

--- onlinemall/src/components/CartStatus.jsx
+++ onlinemall/src/components/CartStatus.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import {AiOutlineShoppingCart} from 'react-icons/ai';
-//import {getCart} from '../api/firebase';
-//import {useQuery} from '@tanstack/react-query';
-//import {useAuthContext} from '../context/AuthContext';
 import useCart from '../hooks/useCart';
 
 export default function CartStatus() {
